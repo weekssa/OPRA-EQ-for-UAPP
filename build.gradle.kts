7c232cd35bb4bbea5e8d7483336996f519361f2d
weekssa <31083077+weekssa@users.noreply.github.com>
2026-08-15 18:34:08 -0500

    Enable Kotlin serialization compiler plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application") version "9.2.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
     id("com.google.devtools.ksp") version "2.3.9" apply false
 }
