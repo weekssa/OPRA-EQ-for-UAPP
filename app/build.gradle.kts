68008d306bc30e95ebd3b7d3a3e72727ed32b6d0
weekssa <31083077+weekssa@users.noreply.github.com>
2026-08-15 18:34:19 -0500

    Apply Kotlin serialization compiler plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.plugin.serialization")
     id("com.google.devtools.ksp")
 }
 
