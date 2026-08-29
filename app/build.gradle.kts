2002f73a801c9f457f7965fafdbe90c20094d2e4
weekssa <31083077+weekssa@users.noreply.github.com>
2026-08-29 07:57:17 -0500

    Bump v0.3 candidate version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.weekssa.opraeqforuapp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     buildTypes {
