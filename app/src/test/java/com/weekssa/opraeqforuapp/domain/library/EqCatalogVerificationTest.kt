cdff40cbad4a2c5c18cef452faeb24b0e12db123
weekssa <31083077+weekssa@users.noreply.github.com>
2026-08-30 17:52:21 -0500

    Use JUnit assertions in verification tests

--- app/src/test/java/com/weekssa/opraeqforuapp/domain/library/EqCatalogVerificationTest.kt
+++ app/src/test/java/com/weekssa/opraeqforuapp/domain/library/EqCatalogVerificationTest.kt
@@ -1,7 +1,7 @@
 package com.weekssa.opraeqforuapp.domain.library
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class EqCatalogVerificationTest {
     @Test
