18e6d22823a9dfce0c5410004c1fb676caa5ae69
weekssa <31083077+weekssa@users.noreply.github.com>
2026-09-14 03:58:41 +0900

    Wire generic Black Pearl device action at activity boundary

--- app/src/main/java/com/weekssa/opraeqforuapp/MainActivity.kt
+++ app/src/main/java/com/weekssa/opraeqforuapp/MainActivity.kt
@@ -87,7 +87,7 @@ class MainActivity : ComponentActivity() {
             resolve(viewModel.resetBlackPearlFromMyDacToFlat())
         },
         onReadBlackPearlQualificationControls = viewModel::readBlackPearlQualificationControls,
-        onSetBlackPearlDacFilter = viewModel::setBlackPearlDacFilter,
+        onSetBlackPearlDeviceControl = viewModel::setBlackPearlDeviceControl,
         onReadFiioJa11DeviceControls = viewModel::readFiioJa11DeviceControls,
         onSetFiioJa11OutputVolume = viewModel::setFiioJa11OutputVolume,
         onSetFiioJa11EqProgram = viewModel::setFiioJa11EqProgram,
