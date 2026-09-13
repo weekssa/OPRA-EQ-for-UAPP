a3837779b5b732f3b388f80a86c0b14fc00e34c5
weekssa <31083077+weekssa@users.noreply.github.com>
2026-09-14 08:18:54 +0900

    Fix Black Pearl controls keyboard options import

--- app/src/main/java/com/weekssa/opraeqforuapp/ui/screens/BlackPearlDeviceBatchControls.kt
+++ app/src/main/java/com/weekssa/opraeqforuapp/ui/screens/BlackPearlDeviceBatchControls.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
@@ -21,7 +22,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.weekssa.opraeqforuapp.domain.blackpearl.BlackPearlDeviceControlReadCodec
