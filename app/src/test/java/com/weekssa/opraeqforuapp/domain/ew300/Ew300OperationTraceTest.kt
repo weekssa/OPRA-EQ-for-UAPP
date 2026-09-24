8f88a7dfb7b2d47e90f906e2addd3e5b37c46304
weekssa <weekssa@gmail.com>
2026-09-24 00:41:00 -0500

    Fix EW300 trace test import

--- app/src/test/java/com/weekssa/opraeqforuapp/domain/ew300/Ew300OperationTraceTest.kt
+++ app/src/test/java/com/weekssa/opraeqforuapp/domain/ew300/Ew300OperationTraceTest.kt
@@ -1,6 +1,7 @@
 package com.weekssa.opraeqforuapp.domain.ew300
 
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
